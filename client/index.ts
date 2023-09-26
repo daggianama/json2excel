757355e4ebc09b5d4615d55a13aafac18f942ed5
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-09-26 12:06:36 +0200

    trim bug solved

--- client/index.ts
+++ client/index.ts
@@ -75,79 +75,114 @@
 import * as XLSX from "xlsx";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const jsonInput1 = document.getElementById(
-        "jsonInput1"
-    ) as HTMLTextAreaElement;
+	const jsonInput1 = document.getElementById(
+		"jsonInput1"
+	) as HTMLTextAreaElement;
 
-    const jsonInput2 = document.getElementById(
-        "jsonInput2"
-    ) as HTMLTextAreaElement;
+	const jsonInput2 = document.getElementById(
+		"jsonInput2"
+	) as HTMLTextAreaElement;
 
-    const jsonInput3 = document.getElementById(
-        "jsonInput3"
-    ) as HTMLTextAreaElement;
+	const jsonInput3 = document.getElementById(
+		"jsonInput3"
+	) as HTMLTextAreaElement;
 
-    const processButton = document.getElementById(
-        "processButton"
-    ) as HTMLButtonElement | null;
+	const processButton = document.getElementById(
+		"processButton"
+	) as HTMLButtonElement | null;
 
-    if (processButton) {
-        processButton.addEventListener("click", async (event) => {
-            const jsonText1 = jsonInput1.value;
-            const jsonText2 = jsonInput2.value;
-            const jsonText3 = jsonInput3.value;
+	if (processButton) {
+		processButton.addEventListener("click", async (event) => {
+			const jsonText1 = jsonInput1.value;
+			const jsonText2 = jsonInput2.value;
+			const jsonText3 = jsonInput3.value;
 
-            try {
-                // Elimina comas, comillas y divide el texto por líneas
-                const lines1 = jsonText1.replace(/,/g, '').replace(/"/g, '').split('\n');
-                const lines2 = jsonText2.replace(/,/g, '').replace(/"/g, '').split('\n');
-                const lines3 = jsonText3.replace(/,/g, '').replace(/"/g, '').split('\n');
-                
-                const filteredData: Record<string, string>[] = [];
+			try {
+				// Elimina comas, comillas y divide el texto por líneas
+				const lines1 = jsonText1
+					.replace(/,/g, "")
+					.replace(/"/g, "")
+					.split("\n");
+				const lines2 = jsonText2
+					.replace(/,/g, "")
+					.replace(/"/g, "")
+					.split("\n");
+				const lines3 = jsonText3
+					.replace(/,/g, "")
+					.replace(/"/g, "")
+					.split("\n");
 
-                for (let i = 0; i < lines1.length; i++) {
-                    const trimmedLine1 = lines1[i].trim();
-                    if (trimmedLine1.length === 0 || trimmedLine1.includes('.comment')) {
-                        // Ignora líneas en blanco o con '.comment'
-                        continue;
-                    }
+				const filteredData: Record<string, string>[] = [];
 
-                    const lineWithoutBrackets = trimmedLine1.replace(/[{}]/g, '');
+				const maxLength = Math.max(
+					lines1.length,
+					lines2.length,
+					lines3.length
+				);
 
-                    const [key, value] = lineWithoutBrackets.split(":").map((item) => item.trim());
-                    
-                    const value2 = lines2[i] ? lines2[i].split(":")[1].trim() : '';
-                    const value3 = lines3[i] ? lines3[i].split(":")[1].trim() : '';
-                    
-                    filteredData.push({ "Nombre": key, "Valor1": value, "Valor2": value2, "Valor3": value3 });
-                }
+				for (let i = 0; i < maxLength; i++) {
+					const trimmedLine1 = lines1[i] ? lines1[i].trim() : "";
+					const trimmedLine2 = lines2[i] ? lines2[i].trim() : "";
+					const trimmedLine3 = lines3[i] ? lines3[i].trim() : "";
 
-                // Crea un libro de Excel
-                const ws = XLSX.utils.json_to_sheet(filteredData);
-                const wb = XLSX.utils.book_new();
-                XLSX.utils.book_append_sheet(wb, ws, "Sheet 1");
+					if (
+						trimmedLine1.length === 0 ||
+						trimmedLine1.includes(".comment") ||
+						trimmedLine1.startsWith("//") ||
+						trimmedLine1.includes(
+							"//======================================================================================================"
+						)
+					) {
+						// Ignora líneas en blanco, con '.comment', que comienzan con '//', o contienen '//======================================================================================================' en el jsonText1
+						continue;
+					}
 
-                // Genera un nombre de archivo único para el Excel
-                const uniqueFileName = "output.xlsx";
+					const lineWithoutBrackets1 = trimmedLine1.replace(
+						/[{}]/g,
+						""
+					);
+					const [key1, value1] = lineWithoutBrackets1
+						.split(":")
+						.map((item) => item.trim());
 
-                // Descarga el archivo Excel
-                XLSX.writeFile(wb, uniqueFileName);
+					const value2 = trimmedLine2.split(":")[1]
+						? trimmedLine2.split(":")[1].trim()
+						: "";
+					const value3 = trimmedLine3.split(":")[1]
+						? trimmedLine3.split(":")[1].trim()
+						: "";
 
-            } catch (error) {
-                if (error instanceof Error) {
-                    console.error("Error al procesar el archivo JSON:", error);
-                    alert(
-                        "Error al procesar el archivo JSON: " + error.message
-                    );
-                } else {
-                    console.error("Error al procesar el archivo JSON:", error);
-                    alert(
-                        "Error al procesar el archivo JSON: " + String(error)
-                    );
-                }
-            }
-        });
-    }
+					filteredData.push({
+						Nombre: key1,
+						Valor1: value1,
+						Valor2: value2,
+						Valor3: value3,
+					});
+				}
+
+				// Crea un libro de Excel
+				const ws = XLSX.utils.json_to_sheet(filteredData);
+				const wb = XLSX.utils.book_new();
+				XLSX.utils.book_append_sheet(wb, ws, "Sheet 1");
+
+				// Genera un nombre de archivo único para el Excel
+				const uniqueFileName = "output.xlsx";
+
+				// Descarga el archivo Excel
+				XLSX.writeFile(wb, uniqueFileName);
+			} catch (error) {
+				if (error instanceof Error) {
+					console.error("Error al procesar el archivo JSON:", error);
+					alert(
+						"Error al procesar el archivo JSON: " + error.message
+					);
+				} else {
+					console.error("Error al procesar el archivo JSON:", error);
+					alert(
+						"Error al procesar el archivo JSON: " + String(error)
+					);
+				}
+			}
+		});
+	}
 });
-
-
